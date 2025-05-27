e8e79e501385b9e62344d4285e3faac44dc4a7e2
Sayantan Mandal <msayantan05@gmail.com>
2025-05-27 11:41:05 +0530

    y

--- backend/utils/model_loader.py
+++ backend/utils/model_loader.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import gdown
+import os
 
 model = None
-class_names = [chr(i) for i in range(65, 65+26)] + ['del', 'nothing', 'space']  # A-Z + 3 extras (adjust if needed)
+class_names = [chr(i) for i in range(65, 65 + 26)] + ['del', 'nothing', 'space']  # A-Z + extras
+
+def download_weights():
+    if not os.path.exists("sign_language_model.h5"):
+        url = "https://drive.google.com/uc?id=1V-kGImLFw7Ayn0Jx8ORuy2fydJ4PODK7"
+        output = "sign_language_model.h5"
+        gdown.download(url, output, quiet=False)
 
 def load_model():
     global model
+    download_weights()  # Ensure model is downloaded
     model = tf.keras.models.load_model("sign_language_model.h5")
     return model
 
